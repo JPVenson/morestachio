7e1a9de36267085e43f8e0faa5f83f7a582c842b
Jean-Pierre Bachmann <Jean-Pierre_Bachmann@live.de>
2020-07-01 20:10:09 +0200

    Added Autocomplete for Edtior

---
+++ Morestachio.NetFramework.Wpf.Editor/JPB.Mustachio.Client.Wpf/ViewModels/MorestachioFoldingStrategy.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Folding;
+using Morestachio.Document;
+using Morestachio.Document.Contracts;
+using Morestachio.Document.Visitor;
+
+namespace JPB.Mustachio.Client.Wpf.ViewModels
+{
+	public class MorestachioFoldingStrategy
+	{
+		public static void UpdateFolding(FoldingManager manager, TextDocument document, IDocumentItem morestachioDocument)
+		{
+			var foldings = CreateNewFoldings(document, morestachioDocument);
+			manager.UpdateFoldings(foldings, -1);
+		}
+
+		private static IEnumerable<NewFolding> CreateNewFoldings(TextDocument document,
+			IDocumentItem morestachioDocument)
+		{
+			var morestachioDocumentChildren = morestachioDocument
+				.Children
+				.Where(e => e.ExpressionStart != null)
+				.ToArray();
+			for (var index = 0; index < morestachioDocumentChildren.Length; index++)
+			{
+				var morestachioDocumentChild = morestachioDocumentChildren[index];
+				var start = document.GetOffset(
+					morestachioDocumentChild.ExpressionStart.Line,
+					morestachioDocumentChild.ExpressionStart.Character - 2);
+
+				var end = document.Text.Length;
+				if (index + 1 < morestachioDocumentChildren.Length)
+				{
+					var documentChild = morestachioDocumentChildren[index + 1];
+					end = document.GetOffset(documentChild.ExpressionStart.Line,
+						documentChild.ExpressionStart.Character);
+				}
+
+				var builder = new ToParsableStringDocumentVisitor();
+
+				if (morestachioDocumentChild.Children.Any())
+				{
+					if (morestachioDocumentChild is ExpressionScopeDocumentItem expScope)
+					{
+						builder.Visit(expScope);
+					}
+					else if (morestachioDocumentChild is EachDocumentItem eachScope)
+					{
+						builder.Visit(eachScope);
+					}
+					else if (morestachioDocumentChild is DoLoopDocumentItem doScope)
+					{
+						builder.Visit(doScope);
+					}
+					else if (morestachioDocumentChild is IfExpressionScopeDocumentItem ifScope)
+					{
+						builder.Visit(ifScope);
+					}
+					else if (morestachioDocumentChild is IfNotExpressionScopeDocumentItem ifNotScope)
+					{
+						builder.Visit(ifNotScope);
+					}
+					else if (morestachioDocumentChild is InvertedExpressionScopeDocumentItem invertScope)
+					{
+						builder.Visit(invertScope);
+					}
+					else if (morestachioDocumentChild is PartialDocumentItem partial)
+					{
+						builder.Visit(partial);
+					}
+					else if (morestachioDocumentChild is WhileLoopDocumentItem whileScope)
+					{
+						builder.Visit(whileScope);
+					}
+
+					yield return new NewFolding(start, end)
+					{
+						Name = builder.StringBuilder.ToString(),
+					};
+				}
+
+				foreach (var newFolding in CreateNewFoldings(document, morestachioDocumentChild))
+				{
+					yield return newFolding;
+				}
+			}
+		}
+	}
+}
--- Morestachio.NetFramework.Wpf.Editor/JPB.Mustachio.Client.Wpf/ViewModels/TemplateEditorViewModel.cs
+++ Morestachio.NetFramework.Wpf.Editor/JPB.Mustachio.Client.Wpf/ViewModels/TemplateEditorViewModel.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Xml;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.CodeCompletion;
 using ICSharpCode.AvalonEdit.Document;
 using ICSharpCode.AvalonEdit.Editing;
+using ICSharpCode.AvalonEdit.Folding;
 using ICSharpCode.AvalonEdit.Highlighting;
 using ICSharpCode.AvalonEdit.Highlighting.Xshd;
 using JPB.Mustachio.Client.Wpf.Services;
@@ -54,6 +55,106 @@ namespace JPB.Mustachio.Client.Wpf.ViewModels
 
 			_templateServiceProvider.TemplateCreated += TemplateServiceProviderOnTemplateCreated;
 		}
+		CompletionWindow _completionWindow;
+
+
+		private void TextArea_TextEntered(object sender, System.Windows.Input.TextCompositionEventArgs e)
+		{
+			if (TextArea.Caret.Offset < 2)
+			{
+				return;
+			}
+			var caret = Template.GetText(TextArea.Caret.Offset - 2, 2);
+			if (caret == "{{")
+			{
+				_completionWindow = new CompletionWindow(TextArea);
+				var data = _completionWindow.CompletionList.CompletionData;
+				data.Add(new SyntaxCompletionData("Do Loop", @"#DO $selection_start$condition$selection_end$}}
+
+{{/DO}}"));
+				data.Add(new SyntaxCompletionData("Each", @"#EACH $selection_start$Data$selection_end$}}
+
+{{/EACH}}"));
+				data.Add(new SyntaxCompletionData("Variable", @"#VAR $selection_start$name$selection_end$ = expression}}"));	
+				data.Add(new SyntaxCompletionData("If", @"#IF $selection_start$condition$selection_end$}}
+
+{{/IF}}"));		
+				data.Add(new SyntaxCompletionData("If Not", @"^IF $selection_start$condition$selection_end$}}
+
+{{/IF}}"));
+				data.Add(new SyntaxCompletionData("While", @"#WHILE $selection_start$condition$selection_end$}}
+
+{{/WHILE}}"));
+				data.Add(new SyntaxCompletionData("Scope", @"#$selection_start$path$selection_end$}}
+
+{{/PATH}}"));
+				data.Add(new SyntaxCompletionData("Partial", @"#DECLARE $selection_start$name$selection_end$}}
+
+{{/DECLARE}}"));
+				data.Add(new SyntaxCompletionData("Include Partial", @"#INCLUDE $selection_start$name$selection_end$}}"));
+				data.Add(new SyntaxCompletionData("While Loop", @"#While $selection_start$condition$selection_end$}}
+
+{{/While}}"));
+				_completionWindow.Show();
+				_completionWindow.Closed += delegate {
+					_completionWindow = null;
+				};
+			}
+		}
+
+		public class SyntaxCompletionData : ICompletionData
+		{
+			private readonly string _text;
+
+			public SyntaxCompletionData(string header, string text)
+			{
+				_text = text;
+				Text = header;
+				Content = header;
+			}
+
+			public const string SEL_START_PLACEHOLDER = "$selection_start$";
+			public const string SEL_END_PLACEHOLDER = "$selection_end$";
+
+			public void Complete(TextArea textArea, ISegment completionSegment, EventArgs insertionRequestEventArgs)
+			{
+				int startIndex = -1;
+				int endIndex = -1;
+				var text = _text;
+				if (text.Contains(SEL_START_PLACEHOLDER))
+				{
+					startIndex = completionSegment.Offset + text.IndexOf(SEL_START_PLACEHOLDER);
+					text = text.Replace(SEL_START_PLACEHOLDER, "");
+
+					if (text.Contains(SEL_END_PLACEHOLDER))
+					{
+						endIndex = completionSegment.Offset + text.IndexOf(SEL_END_PLACEHOLDER);
+						text = text.Replace(SEL_END_PLACEHOLDER, "");
+					}
+				}
+
+				textArea.Document.Replace(completionSegment, text);
+				
+				if (startIndex != -1)
+				{
+					if (endIndex != -1)
+					{
+						textArea.Caret.Position = new TextViewPosition(textArea.Document.GetLocation(startIndex));
+						textArea.Selection = Selection.Create(textArea, startIndex, endIndex);
+					}
+					else
+					{
+						textArea.Caret.Position = new TextViewPosition(textArea.Document.GetLocation(startIndex));
+					}
+				}
+			}
+
+			public ImageSource Image { get; }
+			public string Text { get; }
+			public object Content { get; }
+			public object Description { get; }
+			public double Priority { get; }
+		}
 
 		private void TemplateServiceProviderOnTemplateCreated(object sender, GeneratedTemplateInfos e)
 		{
@@ -62,10 +163,19 @@ namespace JPB.Mustachio.Client.Wpf.ViewModels
 				TextMarkerService.RemoveAll(f => true);
 				foreach (var morestachioError in e.Errors ?? new IMorestachioError[0])
 				{
-					var charOffset = Template.GetOffset(morestachioError.Location.Line, Math.Max(morestachioError.Location.Snipped.Snipped.Length - morestachioError.Location.Character - 1, morestachioError.Location.Character - 1));
-					var textMarker = TextMarkerService.Create(charOffset, morestachioError.Location.Snipped.Snipped.Length);
+					//var column = Math.Max(
+					//	morestachioError.Location.Snipped.Snipped.Length - morestachioError.Location.Character - 1,
+					//	morestachioError.Location.Character - 1);
+					var charOffset = Template.GetOffset(morestachioError.Location.Line,
+						morestachioError.Location.Character - 1);
+					var textMarker = TextMarkerService.Create(charOffset, 1);
 					textMarker.MarkerColor = Colors.Red;
 					textMarker.MarkerTypes = TextMarkerTypes.SquigglyUnderline;
+				}
+
+				if (e.InferredTemplateModel != null)
+				{
+					MorestachioFoldingStrategy.UpdateFolding(FoldingManager, Template, e.InferredTemplateModel);
 				}
 			});
 		}
@@ -124,8 +234,14 @@ namespace JPB.Mustachio.Client.Wpf.ViewModels
 				_textArea = value;
 				TextArea.TextView.BackgroundRenderers.Add(TextMarkerService);
 				TextArea.TextView.LineTransformers.Add(TextMarkerService);
+				TextArea.TextEntered += TextArea_TextEntered;
+
+
+				FoldingManager = FoldingManager.Install(TextArea);
 			}
 		}
+
+		public FoldingManager FoldingManager { get; set; }
 
 		protected virtual void OnTemplateChanged(string e)
 		{
