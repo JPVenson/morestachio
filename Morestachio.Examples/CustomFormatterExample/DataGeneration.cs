37f1793930da4977e175f4be806369c02529ef51
Jean-Pierre Bachmann <Jean-Pierre_Bachmann@live.de>
2020-07-27 14:05:38 +0200

    Added Examples

---
+++ Morestachio.Examples/CustomFormatterExample/DataGeneration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Morestachio.Formatter.Framework;
+
+namespace Morestachio.Examples.CustomFormatterExample
+{
+	public static class DataGeneration
+	{
+		//this method can be used to obtain a new ParserOptions
+		//here you could add custom formatters
+		//this method is optional you can safely remove it
+		public static ParserOptions Config(string templateText, Encoding encoding, bool shouldEscape)
+		{
+			var options = new ParserOptions(templateText, null, encoding, shouldEscape);
+			options.Formatters.AddFromType(typeof(DataGeneration));
+			options.Timeout = TimeSpan.FromMinutes(1);
+			return options;
+		}
+
+		[MorestachioGlobalFormatter("HttpGet", "Gets an string value from the url")]
+		private static async Task<string> GetHttpValue(string source)
+		{
+			var httpClient = new HttpClient();
+			return await httpClient.GetStringAsync(source);
+		}
+
+
+		//there must be always a method in the Program class that will be called to obtain the data
+		public static object GetData()
+		{
+			var collection = new List<object>();
+
+			collection.Add("Hello");
+			collection.Add("World");
+
+			var data = new
+			{
+				Data = collection
+			};
+
+			return data;
+		}
+	}
+}
