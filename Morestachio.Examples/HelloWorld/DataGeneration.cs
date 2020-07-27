ad7da6248aaff86ac1cc22b59bd565e58ce7ad0d
Jean-Pierre Bachmann <Jean-Pierre_Bachmann@live.de>
2020-07-27 11:46:11 +0200

    Added Examples Project

---
+++ Morestachio.Examples/HelloWorld/DataGeneration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+using Morestachio;
+
+namespace Morestachio.Examples.HelloWorld
+{
+	public static class DataGeneration
+	{
+		//this method can be used to obtain a new ParserOptions
+		//here you could add custom formatters
+		//this method is optional you can safely remove it
+		public static ParserOptions Config(string templateText, Encoding encoding, bool shouldEscape)
+		{
+			var options = new ParserOptions(templateText, null, encoding, shouldEscape);
+			options.Timeout = TimeSpan.FromMinutes(1);
+			return options;
+		}
+
+
+		//there must be always a method in the Program class that will be called to obtain the data
+		public static object GetData()
+		{
+			var data = new {
+				Data = new {
+					Value = "Hello World"
+				}
+			};
+
+			return data;
+		}
+	}
+}
