18d8bf1d69adbfbdd2094e67afc77d83834a83aa
JPVenson <info@jean-pierre-bachmann.dev>
2022-01-15 14:22:14 +0100

    Removed stale WPF editor.

--- Morestachio.NetFramework.Wpf.Editor/JPB.Mustachio.Client.Wpf/ViewModels/StoreMetaModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JPB.Mustachio.Client.Wpf.ViewModels
-{
-	public class StoreMetaModel
-	{
-		public string FileName { get; set; }
-		public string Name { get; set; }
-	}
-}
--- Morestachio.NetFramework.Wpf.Editor/JPB.Mustachio.Client.WpfCore/ViewModels/StoreMetaModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JPB.Mustachio.Client.Wpf.Core.ViewModels
-{
-	public class StoreMetaModel
-	{
-		public string FileName { get; set; }
-		public string Name { get; set; }
-	}
-}
