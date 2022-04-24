db015544611c66c7bb72e956e5389a93bb5ab3e6
JPVenson <info@jean-pierre-bachmann.dev>
2022-04-24 18:18:45 +0200

    Fixed Test framework setting Readded Generic tests

--- Morestachio.Tests/GenericsParsingFixture.cs
+++ Morestachio.Tests/GenericsParsingFixture.cs
@@ -2,59 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Morestachio.Document;
 using Morestachio.Formatter.Framework;
+using Morestachio.Formatter.Framework.Attributes;
+using Morestachio.Framework.Expression;
 using NUnit.Framework;
 
 namespace Morestachio.Tests
 {
-	[TestFixture()]
+	[TestFixture]
 	[Parallelizable(ParallelScope.Fixtures)]
-	[Explicit("Currently unable to test. Needs Refactoring!")]
+	//[Explicit("Currently unable to test. Needs Refactoring!")]
 	public class GenericsParsingFixture
 	{
-		public GenericsParsingFixture()
-		{
-
-		}
-
 		public object Result { get; set; }
 
 		private void CallWithResult(string methodName, IDictionary<string, object> values, object expected)
 		{
 			Assert.That(Result, Is.Null);
-			//this method was disabled because MakeGenericMethod is now an instance method
-			//MorestachioFormatterService.PrepareMakeGenericMethodInfoByValues(GetType()
-			//		.GetMethod(methodName), values.Select(e => e.Value).ToArray())
-			//	.Invoke(this, values.Values.ToArray());
-			//Assert.That(Result, Is.EqualTo(expected));
-			//Result = null;
+			var morestachioFormatterService = new MorestachioFormatterService();
+			morestachioFormatterService.AddFromType<GenericsParsingFixture>();
+
+			var parserOptions = new ParserOptions();
+			var types = values.Select((e, i) => new FormatterArgumentType(i, e.Key, e.Value, new MorestachioExpression())).ToArray();
+
+			var cache = morestachioFormatterService.PrepareCallMostMatchingFormatter(GetType(),
+				types,
+				methodName,
+				parserOptions,
+				new ScopeData(parserOptions));
+			
+			morestachioFormatterService.Execute(cache, this, parserOptions, types);
+			Assert.That(Result, Is.EqualTo(expected));
+			Result = null;
 		}
 
-
+		[MorestachioFormatter("[MethodName]", "")]
 		public void SingleGeneric<T>(T value)
 		{
 			Result = value;
 		}
+
 		[Test]
 		public void TestSingleGeneric()
 		{
 			object value = new int[1];
-			CallWithResult(nameof(SingleGeneric), new Dictionary<string, object>()
+
+			CallWithResult(nameof(SingleGeneric), new Dictionary<string, object>
 			{
-				{"value", value},
+				{ "value", value }
 			}, value);
 			value = "Test";
-			CallWithResult(nameof(SingleGeneric), new Dictionary<string, object>()
+
+			CallWithResult(nameof(SingleGeneric), new Dictionary<string, object>
 			{
-				{"value", value},
+				{ "value", value }
 			}, value);
 			value = new object();
-			CallWithResult(nameof(SingleGeneric), new Dictionary<string, object>()
+
+			CallWithResult(nameof(SingleGeneric), new Dictionary<string, object>
 			{
-				{"value", value},
+				{ "value", value }
 			}, value);
 		}
 
+		[MorestachioFormatter("[MethodName]", "")]
 		public void SingleNestedGenericIEnumerable<T>(IEnumerable<T> value)
 		{
 			Result = value;
@@ -64,17 +76,20 @@ namespace Morestachio.Tests
 		public void TestSingleNestedGenericIEnumerable()
 		{
 			object value = new int[1];
-			CallWithResult(nameof(SingleNestedGenericIEnumerable), new Dictionary<string, object>()
+
+			CallWithResult(nameof(SingleNestedGenericIEnumerable), new Dictionary<string, object>
 			{
-				{"value", value},
+				{ "value", value }
 			}, value);
 			value = new List<string>();
-			CallWithResult(nameof(SingleNestedGenericIEnumerable), new Dictionary<string, object>()
+
+			CallWithResult(nameof(SingleNestedGenericIEnumerable), new Dictionary<string, object>
 			{
-				{"value", value},
+				{ "value", value }
 			}, value);
 		}
 
+		[MorestachioFormatter("[MethodName]", "")]
 		public void SingleNestedGenericTuple<T>(Tuple<T> value)
 		{
 			Result = value;
@@ -84,19 +99,22 @@ namespace Morestachio.Tests
 		public void TestSingleNestedGenericTuple()
 		{
 			object value = new Tuple<int>(123);
-			CallWithResult(nameof(SingleNestedGenericTuple), new Dictionary<string, object>()
+
+			CallWithResult(nameof(SingleNestedGenericTuple), new Dictionary<string, object>
 			{
-				{"value", value},
+				{ "value", value }
 			}, value);
 			value = new Tuple<string>("TEST");
-			CallWithResult(nameof(SingleNestedGenericTuple), new Dictionary<string, object>()
+
+			CallWithResult(nameof(SingleNestedGenericTuple), new Dictionary<string, object>
 			{
-				{"value", value},
+				{ "value", value }
 			}, value);
 			value = new Tuple<IEnumerable<string>>(new[] { "test" });
-			CallWithResult(nameof(SingleNestedGenericTuple), new Dictionary<string, object>()
+
+			CallWithResult(nameof(SingleNestedGenericTuple), new Dictionary<string, object>
 			{
-				{"value", value},
+				{ "value", value }
 			}, value);
 		}
 
@@ -109,13 +127,14 @@ namespace Morestachio.Tests
 		[Test]
 		public async Task TestGenericReturnParameter()
 		{
-			var sourceList = new Item[]
+			var sourceList = new[]
 			{
-				new Item() { Key = 1, Value = "A"},
-				new Item() { Key = 2, Value = "B"},
-				new Item() { Key = 1, Value = "C"},
+				new Item { Key = 1, Value = "A" },
+				new Item { Key = 2, Value = "B" },
+				new Item { Key = 1, Value = "C" }
 			};
 			MorestachioDocumentResult result = null;
+
 			await ParserFixture.CreateAndParseWithOptions("{{#VAR result = this.GroupBy((e) => e.Key).ToArray()}}",
 				sourceList,
 				ParserOptionTypes.Precompile,
