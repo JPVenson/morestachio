7ba09841abaebe820c0dde3faaafb1c1f9661296
JPVenson <info@jean-pierre-bachmann.dev>
2022-06-15 12:20:48 +0200

    Added additional Logger setup methods

--- Morestachio.Extensions.Logging/LoggerExtensions.cs
+++ Morestachio.Extensions.Logging/LoggerExtensions.cs
@@ -1,8 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
-using System.Text;
 using Microsoft.Extensions.Logging;
 using MsILogger = Microsoft.Extensions.Logging.ILogger;
-using MoresILogger = Morestachio.Helper.Logging.ILogger;
+using ILogger = Morestachio.Helper.Logging.ILogger;
 
 namespace Morestachio.Extensions.Logging
 {
@@ -11,6 +10,9 @@ namespace Morestachio.Extensions.Logging
 	/// </summary>
 	public static class LoggerExtensions
 	{
+		/// <summary>
+		///		Adds an Adapter for <see cref="MsILogger"/> to <see cref="ILogger"/>
+		/// </summary>
 		public static IParserOptionsBuilder WithMicrosoftLogger(this IParserOptionsBuilder builder,
 																MsILogger logger,
 																int eventId,
@@ -19,106 +21,26 @@ namespace Morestachio.Extensions.Logging
 		{
 			return builder.WithLogger(() => new MicrosoftLoggerAdapter(logger, eventId, defaultLogLevel, format));
 		}
-	}
 
-	public class MicrosoftLoggerAdapter : MoresILogger
-	{
-		private readonly ILogger _logger;
-		private readonly int _eventId;
-		private readonly LogLevel _defaultLogLevel;
-		private readonly Func<LogEntry, string> _format;
-
-		public MicrosoftLoggerAdapter(
-			ILogger logger,
-			int eventId,
-			LogLevel defaultLogLevel,
-			Func<LogEntry, string> format
-		)
+		/// <summary>
+		///		Adds an Adapter for <see cref="MsILogger"/> to <see cref="ILogger"/>
+		/// </summary>
+		public static IParserOptionsBuilder WithMicrosoftLogger(this IParserOptionsBuilder builder,
+																MsILogger logger,
+																int eventId,
+																LogLevel defaultLogLevel)
 		{
-			_logger = logger;
-			_eventId = eventId;
-			_defaultLogLevel = defaultLogLevel;
-			_format = format;
-			Enabled = true;
+			return builder.WithMicrosoftLogger(logger, eventId, defaultLogLevel, null);
 		}
 
-		/// <inheritdoc />
-		public bool Enabled { get; set; }
-
-		public class LogEntry
+		/// <summary>
+		///		Adds an Adapter for <see cref="MsILogger"/> to <see cref="ILogger"/>
+		/// </summary>
+		public static IParserOptionsBuilder WithMicrosoftLogger(this IParserOptionsBuilder builder,
+																MsILogger logger,
+																int eventId)
 		{
-			public string Message { get; }
-			public IDictionary<string, object> Data { get; }
-
-			public LogEntry(string message, IDictionary<string, object> data)
-			{
-				Message = message;
-				Data = data;
-			}
-		}
-
-		/// <inheritdoc />
-		public void Log(
-			string logLevel,
-			string eventId,
-			string message,
-			IDictionary<string, object> data
-		)
-		{
-			if (!Enabled)
-			{
-				return;
-			}
-
-			_logger.Log(ParseLogLevel(logLevel), new EventId(_eventId, eventId), new LogEntry(message, data), null, FormatMessage);
-		}
-
-		/// <inheritdoc />
-		public void Log(string logLevel, string eventId, string message)
-		{
-			if (!Enabled)
-			{
-				return;
-			}
-
-			_logger.Log(ParseLogLevel(logLevel), new EventId(_eventId, eventId), new LogEntry(message, null), null, FormatMessage);
-		}
-
-		private string FormatMessage(LogEntry entry, Exception? arg2)
-		{
-			if (_format != null)
-			{
-				return _format(entry);
-			}
-
-			if (entry.Data?.Count is null or 0)
-			{
-				return entry.Message;
-			}
-
-			var sb = new StringBuilder(entry.Message);
-			sb.AppendLine();
-
-			foreach (var data in entry.Data)
-			{
-				sb.AppendLine($"{data.Key} - {(data.Value?.ToString() ?? "{NULL}")}");
-			}
-
-			return sb.ToString();
-		}
-
-		private LogLevel ParseLogLevel(string logLevel)
-		{
-			return logLevel.ToUpper() switch
-			{
-				"Warning" => LogLevel.Warning,
-				"Critical" => LogLevel.Critical,
-				"Debug" => LogLevel.Debug,
-				"Error" => LogLevel.Error,
-				"Info" => LogLevel.Information,
-				"Trace" => LogLevel.Trace,
-				_ => _defaultLogLevel
-			};
+			return builder.WithMicrosoftLogger(logger, eventId, LogLevel.Debug);
 		}
 	}
 }
---
+++ Morestachio.Extensions.Logging/MicrosoftLoggerAdapter.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+using Microsoft.Extensions.Logging;
+using ILogger = Morestachio.Helper.Logging.ILogger;
+
+namespace Morestachio.Extensions.Logging;
+
+public class MicrosoftLoggerAdapter : ILogger
+{
+	private readonly Microsoft.Extensions.Logging.ILogger _logger;
+	private readonly int _eventId;
+	private readonly LogLevel _defaultLogLevel;
+	private readonly Func<LogEntry, string> _format;
+
+	public MicrosoftLoggerAdapter(
+		Microsoft.Extensions.Logging.ILogger logger,
+		int eventId,
+		LogLevel defaultLogLevel,
+		Func<LogEntry, string> format
+	)
+	{
+		_logger = logger;
+		_eventId = eventId;
+		_defaultLogLevel = defaultLogLevel;
+		_format = format;
+		Enabled = true;
+	}
+
+	/// <inheritdoc />
+	public bool Enabled { get; set; }
+
+	/// <summary>
+	///		Defines an log entry
+	/// </summary>
+	public class LogEntry
+	{
+		public string Message { get; }
+		public IDictionary<string, object> Data { get; }
+
+		public LogEntry(string message, IDictionary<string, object> data)
+		{
+			Message = message;
+			Data = data;
+		}
+	}
+
+	/// <inheritdoc />
+	public void Log(
+		string logLevel,
+		string eventId,
+		string message,
+		IDictionary<string, object> data
+	)
+	{
+		if (!Enabled)
+		{
+			return;
+		}
+
+		_logger.Log(ParseLogLevel(logLevel), new EventId(_eventId, eventId), new LogEntry(message, data), null, FormatMessage);
+	}
+
+	/// <inheritdoc />
+	public void Log(string logLevel, string eventId, string message)
+	{
+		if (!Enabled)
+		{
+			return;
+		}
+
+		_logger.Log(ParseLogLevel(logLevel), new EventId(_eventId, eventId), new LogEntry(message, null), null, FormatMessage);
+	}
+
+	private string FormatMessage(LogEntry entry, Exception? arg2)
+	{
+		if (_format != null)
+		{
+			return _format(entry);
+		}
+
+		if (entry.Data?.Count is null or 0)
+		{
+			return entry.Message;
+		}
+
+		var sb = new StringBuilder(entry.Message);
+		sb.AppendLine();
+
+		foreach (var data in entry.Data)
+		{
+			sb.AppendLine($"{data.Key} - {(data.Value?.ToString() ?? "{NULL}")}");
+		}
+
+		return sb.ToString();
+	}
+
+	private LogLevel ParseLogLevel(string logLevel)
+	{
+		return logLevel.ToUpper() switch
+		{
+			"Warning" => LogLevel.Warning,
+			"Critical" => LogLevel.Critical,
+			"Debug" => LogLevel.Debug,
+			"Error" => LogLevel.Error,
+			"Info" => LogLevel.Information,
+			"Trace" => LogLevel.Trace,
+			_ => _defaultLogLevel
+		};
+	}
+}
